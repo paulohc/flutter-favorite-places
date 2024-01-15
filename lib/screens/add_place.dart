3dc5993f5100625749307da7a4d88f50c1de0ecf
Paulo Henrique Carvalho de Morais <paulohcarvalho9@gmail.com>
2024-01-15 10:22:20 -0300

    feat: create "Add Place" screen

---
+++ lib/screens/add_place.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class AddPlacesScreen extends StatefulWidget {
+  const AddPlacesScreen({super.key});
+
+  @override
+  State<AddPlacesScreen> createState() {
+    return _AddPlaceScreenState();
+  }
+}
+
+class _AddPlaceScreenState extends State<AddPlacesScreen> {
+  final _titleController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add new Place'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+              label: const Text('Add Place'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
