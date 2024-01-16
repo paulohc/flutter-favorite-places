a062c381e52351fe632ebf7aea69db94075e3920
Paulo Henrique Carvalho de Morais <paulohcarvalho9@gmail.com>
2024-01-16 00:31:35 -0300

    feat: store place in a SQL database

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -4,6 +4,8 @@ import 'package:favorite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
@@ -18,6 +20,29 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
       image: copiedImage,
       location: location,
     );
+
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)',
+        );
+      },
+      version: 1,
+    );
+    db.insert(
+      'user_places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+        'lat': newPlace.location.latitude,
+        'lng': newPlace.location.longitude,
+        'address': newPlace.location.address,
+      },
+    );
+
     state = [newPlace, ...state];
   }
 }
